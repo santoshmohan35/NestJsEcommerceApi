12c6e12b29e6008b8662eaf50ffccb1aa04b280a
Santosh_Mohan <santosh.mohan@unvired.io>
2020-08-28 16:49:25 +0530

    password encryption added

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,25 +1,32 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { Users } from './users.model';
-import { Observable } from 'rxjs';
 import { UserLoginDto } from 'src/models/userLogin.model';
+import { hash, compare } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
     constructor(private prisma: PrismaService) { }
 
-    createUser(user: Users) {
-        const data = this.prisma.user.create({
+    async createUser(user: Users) {
+        const hashPassword = await hash(user.password, 10);
+        const data = await this.prisma.user.create({
             data: {
                 firstname: user.firstname,
                 lastname: user.lastname,
                 email: user.email,
-                password: user.password,
+                password: hashPassword,
                 mobile: user.mobile
             }
         });
         return data;
     }
+
+    async comparePassword(plainPassword, hashPassword) {
+        const result = await compare(plainPassword, hashPassword);
+        return result;
+    }
+
     async getAllUsers() {
         return await this.prisma.user.findMany({
             select: {
@@ -41,12 +48,12 @@ export class UsersService {
         if (!result) {
             throw new HttpException('No such user found', HttpStatus.BAD_REQUEST);
         }
-        if( result.password !== user.password) {
+        const comparePassword = await this.comparePassword(user.password, result.password);
+        if (!comparePassword) {
             throw new HttpException('Invalid Password', HttpStatus.UNAUTHORIZED);
         } else {
             return result;
         }
-        // const result = await this.prisma.$queryRaw`SELECT * FROM user where email = ${user.username} && password = ${user.password}`;
     }
 
     async findByPayload(payload: any) {
